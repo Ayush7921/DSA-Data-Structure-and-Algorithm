8670af66c307d2e3a7297b263fa7cb027a9f8082
Ayush Chaudhary || DTU  IT'28 <ayush79211@gmail.com>
2026-06-30 15:11:56 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (45.21%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,38 +1,21 @@
 class MyStack {
 public:
-    queue<int> q1;
-    queue<int> q2;
+    queue<int> q;
     MyStack() {
         
     }
     
     void push(int x) {
-        if(q1.empty()){
-            q1.push(x);
-        }
-        else{
-
-        while(!q1.empty()){
-            q2.push(q1.front());
-            q1.pop();
-        }
-        q1.push(x);
-
-        while(!q2.empty()){
-            q1.push(q2.front());
-            q2.pop();
-        }
-
+        q.push(x);
+        for(int i = 0 ; i< q.size()-1 ; i++){
+            q.push(q.front());
+            q.pop();
         }
     }
     
     int pop() {
-        if(q1.empty()){
-            return -1;
-        }
-
-        int x = q1.front();
-        q1.pop();
+        int x = q.front();
+        q.pop();
 
         return x;
         
@@ -40,13 +23,13 @@ public:
     
     int top() {
 
-        return q1.front();
+        return q.front();
         
     }
     
     bool empty() {
 
-        return q1.empty();
+        return q.empty();
         
     }
 };
