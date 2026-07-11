94abb06cbee79becd2c3623e710331fca17e3114
Ayush Chaudhary || DTU  IT'28 <ayush79211@gmail.com>
2026-07-11 16:46:39 +0530

    Time: 75 ms (49.48%) | Memory: 14.5 MB (48.36%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    int solve (vector<int >& nums , vector<vector<int> >& dp,int  idx, int prev,int n){
-        if(idx>=n){
-            return 0;
-        }
-        if(dp[idx][prev+1]!=-1){
-            return dp[idx][prev+1];
-        }
-        int take = 0;
-
-        if(prev==-1 || nums[prev]<nums[idx]){
-            take = 1 + solve(nums,dp ,idx+1,idx,n);
-        }
-
-        int skip = solve(nums,dp ,idx+1,prev,n);
-        return dp[idx][prev+1]=max(skip,take);
-    }
+   
     int lengthOfLIS(vector<int>& nums) {
 
         int n = nums.size();
-
+        int maxresult =0;
         if(n==1) return 1;
+        vector<int>dp(n,1);
 
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
+        for(int i = 0 ; i<n;i++){
+            for(int j =0 ;j<i;j++ ){
+                if(nums[j]<nums[i]){
+                    dp[i]= max(dp[i], dp[j]+1);
 
-        return solve (nums ,dp,0,-1,n);
+                    maxresult=max(maxresult, dp[i]);
+                }
+
+
+            }
+        }
+        return (maxresult==0) ? 1 : maxresult ;
+
     }
 };
