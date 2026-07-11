3db6da635a5a3b432b247bab651b24f9fbee4453
Ayush Chaudhary || DTU  IT'28 <ayush79211@gmail.com>
2026-07-11 14:04:50 +0530

    Time: 105 ms (97.18%) | Memory: 94.7 MB (95.22%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     long long maxAlternatingSum(vector<int>& nums) {
+        long long sumAdd = 0; // Tracks the state where the last operation was an addition
+        long long sumSub = 0; // Tracks the state where the last operation was a subtraction
 
-      int n = nums.size();
-      vector< vector<long>> dp(n+1 , vector<long> (2,0));
+        for (int x : nums) {
+            long long tempAdd = max(sumAdd, sumSub + x);
+            long long tempSub = max(sumSub, sumAdd - x);
+            
+            sumAdd = tempAdd;
+            sumSub = tempSub;
+        }
 
-      for(int i =1 ; i<=n ; i++){
-        dp[i][0]= max(dp[i-1][1] - nums[i-1] , dp[i-1][0]);
-        dp[i][1]= max(dp[i-1][0] + nums[i-1] , dp[i-1][1]);
-      }
-
-      return max(dp[n][0], dp[n][1]);
+        return max(sumAdd, sumSub);
     }
 };
