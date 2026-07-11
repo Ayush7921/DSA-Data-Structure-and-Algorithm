65127dfcfdd8c09887b7ceb6f7dcf957d509749e
Ayush Chaudhary || DTU  IT'28 <ayush79211@gmail.com>
2026-07-11 14:03:29 +0530

    Time: 259 ms (45.22%) | Memory: 157 MB (48.37%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -1,32 +1,15 @@
 class Solution {
 public:
-
-    long long dp[100001][2];
-
-    long long solve(vector<int>& nums,int i,bool flag,int n){
-
-        if(i>=n)
-            return 0;
-
-        if(dp[i][flag]!=-1)
-            return dp[i][flag];
-
-        long long skip = solve(nums,i+1,flag,n);
-
-        long long val = nums[i];
-
-        if(!flag)
-            val = -val;
-
-        long long take = val + solve(nums,i+1,!flag,n);
-
-        return dp[i][flag] = max(skip,take);
-    }
-
     long long maxAlternatingSum(vector<int>& nums) {
 
-        memset(dp,-1,sizeof(dp));
+      int n = nums.size();
+      vector< vector<long>> dp(n+1 , vector<long> (2,0));
 
-        return solve(nums,0,1,nums.size());
+      for(int i =1 ; i<=n ; i++){
+        dp[i][0]= max(dp[i-1][1] - nums[i-1] , dp[i-1][0]);
+        dp[i][1]= max(dp[i-1][0] + nums[i-1] , dp[i-1][1]);
+      }
+
+      return max(dp[n][0], dp[n][1]);
     }
 };
