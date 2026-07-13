9e02c99ecb12a4b9429404f659fa9f457ed4f983
Ayush Chaudhary || DTU  IT'28 <ayush79211@gmail.com>
2026-07-13 12:56:56 +0530

    Time: 34 ms (88.74%) | Memory: 18.7 MB (69.38%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-    int dp[1001][1001];
 
     bool isSubsequence(string &s1, string &s2) {
         int i = 0; 
@@ -14,32 +13,27 @@ public:
     }
         return i == s1.length();
     }
-    
-    int solve(vector<string>& words , int i , int p , int n ){
-        if(i>=n){
-            return 0;
-        }
-
-        if(dp[i][p+1]!=-1){
-            return dp[i][p+1];
-        }
-        
-        int len = solve (words , i+1 , p , n); 
-        if(p==-1 || words[i].size()==words[p].size() +1 && isSubsequence(words[p],words[i])){
-
-            len = max(len , 1 + solve(words , i+1 , i , n));
-
-        }
-
-        return dp[i][p+1]= len ; 
-    }
 
     int longestStrChain(vector<string>& words) {
         
-        memset(dp,-1 , sizeof(dp));
+    
         sort(words.begin(), words.end(), [](const string& a, const string& b) {
         return a.size() < b.size(); 
         });
-        return solve(words,0,-1,words.size());
+        vector<int > dp(words.size(),1);
+        int maxlen= 1;
+        for(int i =0 ; i<words.size() ; i++){
+            for(int j =0 ; j<i ; j++){
+
+                if(  words[i].size()==words[j].size()+1 && isSubsequence(words[j],words[i])  ){
+                    
+                    dp[i] = max(dp[i],dp[j]+1);
+                    maxlen = max(maxlen ,dp[i] );
+                }
+
+            }
+        }
+
+        return maxlen;
     }
 };
