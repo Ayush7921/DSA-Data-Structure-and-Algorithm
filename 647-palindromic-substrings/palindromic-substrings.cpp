8b3f9bb5681881b30783e8983ddda31e11937d45
Ayush Chaudhary || DTU  IT'28 <ayush79211@gmail.com>
2026-07-16 11:01:59 +0530

    Time: 34 ms (27.29%) | Memory: 11.2 MB (41.90%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    int dp[1001][1001];
-
-    bool ispalindrome(string &s , int i , int j ){
-        if(i>=j){
-            return true ;
-        }
-        
-        if(dp[i][j]!=-1){ return dp[i][j] ; }
-        if(s[i]==s[j]){
-            return dp[i][j] = ispalindrome(s,i+1,j-1);
-        }
-
-        return dp[i][j]= false ;
-    }
-
-
     int countSubstrings(string s) {
         int n = s.size();
+        vector<vector<bool>> dp(n, vector<bool> (n));
+        int count = 0;
+        
+        for(int L = 1 ; L <= n ; L++){
 
-        memset(dp,-1,sizeof(dp));
+            for(int i = 0 ; i+L-1<n ; i++){
+                int j = i+L-1;
 
-        int result=0;
+                if(i==j){
+                    dp[i][i]= true;
+                }
+                else if(i+1==j){
+                    dp[i][j] = (s[i]==s[j]) ;
+                }else{
+                    dp[i][j] = ((s[i]==s[j]) && (dp[i+1][j-1]));
+                }
 
-        for(int i = 0 ; i<n ; i++){
-            for(int j = i ; j< n ; j++){
-
-                if(ispalindrome(s,i,j)){
-                    result+=1;
+                if(dp[i][j]){
+                    count++ ;
                 }
             }
         }
 
-        return result ;
+        return count ;
+        
     }
 };
