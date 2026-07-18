6740a822e17cb819109d04be4936f76058d4534a
Ayush Chaudhary || DTU  IT'28 <ayush79211@gmail.com>
2026-07-18 13:48:40 +0530

    Time: 87 ms (69.69%) | Memory: 240.3 MB (25.23%) - LeetSync

--- 403-frog-jump/frog-jump.cpp
+++ 403-frog-jump/frog-jump.cpp
@@ -1,39 +1,52 @@
 class Solution {
 public:
-    bool solve(vector<int>& stones,unordered_map<int,int >& mp ,int i , int k ,  vector<vector<int>>& dp){
+    bool solve(vector<int>& stones, unordered_map<int,int>& mp, int i, int k, vector<vector<int>>& dp) {
         
-        if (i==stones.size()-1){
-            return true ;
+        // Base case: Reached the end
+        if (i == stones.size() - 1) {
+            return true;
         }
-        if(dp[k][i]!=-1){
-            return (bool)dp[k][i];
+        
+        // Return memoized result
+        if (dp[k][i] != -1) {
+            return dp[k][i];
         }
-        bool a=false;
-        int currpos= stones[i] ; 
-        for(int jump = k-1  ; jump<=k+1 ; jump++){
-
-            if(jump>0 && mp.contains(currpos+jump)){
-                int nextidx =mp[stones[i]+jump];
-                if(solve(stones , mp , nextidx , jump , dp)){
-                    return dp[i][k] =1 ;
-                };
+        
+        int currpos = stones[i]; 
+        
+        // Try all 3 jumps
+        for (int jump = k - 1; jump <= k + 1; jump++) {
+            if (jump > 0 && mp.contains(currpos + jump)) {
+                int nextidx = mp[currpos + jump];
+                
+                // EARLY EXIT: If this path works, return immediately
+                if (solve(stones, mp, nextidx, jump, dp)) {
+                    return dp[k][i] = 1;
+                }
             }
         }
+        
+        // If no jumps worked, mark as false
         return dp[k][i] = 0;
-
     }
+
     bool canCross(vector<int>& stones) {
         int n = stones.size();
-        if(stones[1]!=1){
-            return (stones[1]==1);
+
+        // The frog must be able to reach the first stone
+        if (stones[1] != 1) {
+            return false;
         }
 
-        unordered_map<int,int > mp;
-        for(int i = 0 ; i< n;i++){
-            mp[stones[i]]=i;
+        // Map for O(1) lookups
+        unordered_map<int,int> mp;
+        for (int i = 0; i < n; i++) {
+            mp[stones[i]] = i;
         }
-        vector<vector<int>> dp(n+1,vector<int>(n,-1));
-        return solve(stones , mp , 1,1,dp);
+
+        // Safe DP size: Max jump size is n, max index is n-1
+        vector<vector<int>> dp(n + 1, vector<int>(n, -1));
         
+        return solve(stones, mp, 1, 1, dp);
     }
 };
