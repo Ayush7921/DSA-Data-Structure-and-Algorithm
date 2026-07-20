b460ba118a88b8c219373b9fd13aafb9a09a1869
Ayush Chaudhary || DTU  IT'28 <ayush79211@gmail.com>
2026-07-20 15:00:07 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (61.40%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,48 +1,34 @@
 class Solution {
 public:
-    void dfs(unordered_map<int , vector<int> >& adj , vector<bool> &vis,int i){
-
-        vis[i]=true ;
-
-        for(int &v : adj[i]){
-            if(!vis[v]){
-                dfs(adj,vis,v);
-            }
-        }
-
-    }
 
     int findCircleNum(vector<vector<int>>& isConnected) {
 
-        unordered_map<int , vector<int> > adj ;
-
-        int r= isConnected.size();
-        int c = isConnected[0].size();
-
-        for(int i = 0 ; i< r ; i++){
-            for(int j = i+1 ; j< c ; j++ ){
-                if(isConnected[i][j]==1){
-                    int u = i+1;
-                    int v = j+1;
-
-                    adj[u].push_back(v);
-                    adj[v].push_back(u);
-                }
-            }
-        }
-
-
-        int v = r;
-
+        int v = isConnected.size();
         vector<bool> vis(v+1, false);
 
         int provisions = 0 ;
 
+        queue<int > q ;
         for(int i = 1 ; i<= v ; i++){
-            if(!vis[i]){
-                dfs(adj , vis,i);
-                provisions++ ;
+            if(vis[i]){
+                continue;
             }
+
+            vis[i]=true ;
+            q.push(i);
+            provisions++;
+
+            while(!q.empty()){
+                int u = q.front();
+                q.pop();
+
+                for(int j = 1 ; j<=v; j++ ){
+                    if(!vis[j] && isConnected[u-1][j-1]==1){
+                        vis[j]=true ;
+                        q.push(j);
+                    }
+                }
+            } 
         }
 
         return provisions;
