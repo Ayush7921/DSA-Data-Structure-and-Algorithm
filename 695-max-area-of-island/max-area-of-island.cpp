5fc6d7dbabdc0baed9e4cd4ee11ae6d2ca0760a3
Ayush Chaudhary || DTU  IT'28 <ayush79211@gmail.com>
2026-07-21 22:33:53 +0530

    Time: 5 ms (29.34%) | Memory: 30.9 MB (26.07%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+
+    void bfs(vector<vector<int>>& grid , int row , int col,int &area){
+        queue<pair<int , int>> q ;
+
+        q.push({row,col});
+        grid[row][col]=0;
+
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        int delrow[] = {-1, 1, 0, 0};
+        int delcol[] = {0, 0, -1, 1};
+
+        while(!q.empty()){
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+
+            for(int i = 0 ; i<4 ; i++){          
+                int newrow = r + delrow[i];
+                int newcol = c + delcol[i];
+
+                if((newcol>=0 && newcol<m) && (newrow>=0 && newrow<n) && (grid[newrow][newcol]==1) ){
+                    q.push({newrow,newcol});
+                    grid[newrow][newcol]=0;
+                    area+=1;
+                }
+
+            }
+
+
+        }
+
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+
+        int r = grid.size();
+        int c = grid[0].size();
+        int ans = 0;
+
+        for(int row = 0 ; row < r ; row++){
+            for(int col = 0 ; col < c ; col++ ){
+                if(grid[row][col]==1){
+                    int area=1 ;
+                    bfs(grid,row,col,area);
+                    ans = max(ans , area);
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
