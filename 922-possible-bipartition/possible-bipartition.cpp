05b1a58bd5a7684b4b176cffc88afff19d0ea682
Ayush Chaudhary || DTU  IT'28 <ayush79211@gmail.com>
2026-07-24 11:34:01 +0530

    Time: 47 ms (37.61%) | Memory: 71.4 MB (42.43%) - LeetSync

--- 922-possible-bipartition/possible-bipartition.cpp
+++ 922-possible-bipartition/possible-bipartition.cpp
@@ -1,57 +1,45 @@
 class Solution {
 public:
-    vector<int> parent, Rank;
+    bool dfs(unordered_map<int ,vector< int>> &adj,vector<int > &vis,int currcolor,int u){
+        
+        vis[u]=1-currcolor;
 
-    int find(int x) {
-        if(parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
+        for(int & v : adj[u]){
+            if(vis[v]==-1){
+                if(dfs(adj , vis ,1-currcolor, v )){
+                    return true ;
+                }
+            }else if(vis[u]==vis[v]){
+                return true ;
+            }
+        }
+        
+        return false;
+
+
     }
-
-    void Union(int a, int b) {
-        a = find(a);
-        b = find(b);
-
-        if(a == b) return;
-
-        if(Rank[a] < Rank[b])
-            swap(a, b);
-
-        parent[b] = a;
-
-        if(Rank[a] == Rank[b])
-            Rank[a]++;
-    }
-
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
 
-        vector<vector<int>> adj(n + 1);
+        unordered_map<int , vector<int>> adj ;
 
-        for(auto &e : dislikes) {
+        for(auto &e:dislikes)
+        {
             adj[e[0]].push_back(e[1]);
             adj[e[1]].push_back(e[0]);
+
         }
-
-        parent.resize(n + 1);
-        Rank.assign(n + 1, 0);
-
-        for(int i = 1; i <= n; i++)
-            parent[i] = i;
-
-        for(int u = 1; u <= n; u++) {
-
-            if(adj[u].empty()) continue;
-
-            int first = adj[u][0];
-
-            for(int v : adj[u]) {
-
-                if(find(u) == find(v))
-                    return false;
-
-                Union(first, v);
+        
+        vector<int> vis (n+1,-1);
+        for(int i = 1 ; i<=n ; i++  ){
+            if(vis[i]==-1 && dfs(adj,vis,1,i)){
+                return false ;
             }
         }
 
-        return true;
+        return true ;
+
+
+
+
     }
 };
