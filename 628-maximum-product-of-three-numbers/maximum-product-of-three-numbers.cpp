134b984a7aec9b78bf77913f80a0406c9e179b7f
Ayush Chaudhary || DTU  IT'28 <ayush79211@gmail.com>
2026-07-26 11:39:42 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (96.80%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -2,10 +2,43 @@ class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
 
-        sort(nums.begin(),nums.end());
+        int m1 = INT_MIN;
+        int m2 = INT_MIN;
+        int m3 = INT_MIN;
+        int s1 = INT_MAX;
+        int s2 = INT_MAX;   
 
-        int i = 0 ;
-        int j = nums.size()-1;
-        return  max(nums[i]*nums[i+1]*nums[j] ,  nums[j]*nums[j-1]* nums[j-2]);        
+
+        for(int i = 0 ; i< nums.size() ; i++){
+            if(m1<nums[i])
+            {
+                m3 = m2 ;
+                m2 = m1 ;
+                m1 = nums[i];
+
+            }
+            else if (m2<nums[i])
+            {
+                m3 = m2 ;
+                m2 = nums[i];
+            }
+            else if(m3 < nums[i])
+            {
+                m3 = nums[i];
+            }
+
+
+            if(s1>nums[i]){
+                s2=s1;
+                s1=nums[i];
+            }else if(s2>nums[i]){
+                s2 = nums[i];
+            }
+        }
+
+        int op1 = s1*s2*m1;
+        int op2 = m1*m2*m3 ;
+
+        return max(op1,op2);     
     }
 };
