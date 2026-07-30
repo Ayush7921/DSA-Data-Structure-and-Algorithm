e3b0584a53042d119cbbfbf14f9855604e537e05
Ayush Chaudhary || DTU  IT'28 <ayush79211@gmail.com>
2026-07-30 20:18:27 +0530

    Time: 8 ms (88.26%) | Memory: 22.3 MB (51.20%) - LeetSync

---
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    vector<int> parent ;
+    vector<int> rank;
+
+    int find (int i){
+        if(i==parent[i]){
+            return i ;
+        }
+
+        return parent[i] = find(parent[i]);
+    }
+
+    void Union(int i , int j ){
+        int x = find(i);
+        int y = find(j);
+
+        if(x==y){
+            return ;
+        }
+
+        if(rank[x]>rank[y]){
+            parent[y]=x;
+        }else if (rank[x]>rank[y]){
+            parent[x]=y;
+        }else{
+            parent[y]=x;
+            rank[x]++;
+        }
+    }
+
+    int removeStones(vector<vector<int>>& stones) {
+
+        int mr = 0 ;
+        int mc = 0 ;
+
+        for(auto &c : stones){
+            mr = max(mr , c[0]);
+            mc = max(mc , c[1]);
+        }
+        
+        int n = mr + mc + 2 ;
+        parent.assign(n,0);
+        for(int i = 0 ; i<n ; i++ ){
+            parent[i]=i ;
+        }
+
+        rank.assign(n ,0);
+
+        unordered_map<int , int > mp ;
+
+        for(auto & c : stones ){
+            int r = c[0];
+            int col = mr + 1 + c[1];
+
+            Union(r,col);
+
+            mp[r]=1;
+            mp[col]=1;
+        }
+
+        int c = 0 ;
+        for(auto & it : mp){
+            if(parent[it.first]==it.first){
+                c++;
+            }
+        }
+
+        return stones.size()-c;       
+    }
+};
