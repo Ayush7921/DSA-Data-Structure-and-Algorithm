d9a038a63a5d9ab71358f53f75251cff18eb583f
Ayush Chaudhary || DTU  IT'28 <ayush79211@gmail.com>
2026-08-02 11:41:35 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (46.05%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
 
-    int dp[501][501] ;
-    int solve(int i , int j , vector<int>& piles )
-    {
-        if(i==j) return piles[i];
+    // int dp[501][501] ;
+    // int solve(int i , int j , vector<int>& piles )
+    // {
+    //     if(i==j) return piles[i];
 
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
+    //     if(dp[i][j]!=-1){
+    //         return dp[i][j];
+    //     }
 
-        int t1 = piles[i] - solve(i+1,j,piles);
-        int t2 = piles[j] - solve(i,j-1,piles);
+    //     int t1 = piles[i] - solve(i+1,j,piles);
+    //     int t2 = piles[j] - solve(i,j-1,piles);
 
 
-        return dp[i][j]=max(t1,t2);
-    }
+    //     return dp[i][j]=max(t1,t2);
+    // }
     bool stoneGame(vector<int>& piles) {
 
-        memset(dp,-1,sizeof(dp));
-        return (solve(0,piles.size()-1,piles)>0);
+        // memset(dp,-1,sizeof(dp));
+        // return (solve(0,piles.size()-1,piles)>0);
+
+        return true;
     }
 };
