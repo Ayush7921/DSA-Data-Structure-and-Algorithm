9277f035827cf2189c2c0553f00f51c5fff7fa1f
Ayush Chaudhary || DTU  IT'28 <ayush79211@gmail.com>
2026-08-05 11:54:09 +0530

    Time: 79 ms (71.60%) | Memory: 44.4 MB (53.22%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,50 +1,56 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>> adj(n+1);
+        vector<vector<pair<int,int>>> adj(n + 1);
 
-        for(auto &e  : times){
+        for(auto &e : times){
             int u = e[0];
             int v = e[1];
             int wt = e[2];
 
-            adj[u].push_back({v,wt});
+            adj[u].push_back({v, wt});
         }
 
-        set<pair<int,int >>st;
-        vector<int> d(n+1,INT_MAX);
-        d[k]=0;
-        d[0]=0;
-        st.insert({0,k});
+        // Min-Heap using priority_queue
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        
+        vector<int> d(n + 1, INT_MAX);
+        d[k] = 0;
+        d[0] = 0; // Ignoring the 0th index
+        pq.push({0, k});
 
-        while(!st.empty()){
-            auto it = st.begin();
-            int di = it->first;
-            int node = it->second ;
-            st.erase(it);
+        while(!pq.empty()){
+            // Get the top element and immediately pop it
+            auto it = pq.top();
+            pq.pop();
+            
+            int di = it.first;
+            int node = it.second;
+
+            // LAZY DELETION: 
+            // If we pull out an outdated, longer distance, just ignore it.
+            if(di > d[node]) continue;
 
             for(auto &v : adj[node]){
                 int adjnode = v.first;
-                int weight = v.second ;
+                int weight = v.second;
 
                 if(di + weight < d[adjnode]){
-                    if(d[adjnode] != INT_MAX){
-                        st.erase({d[adjnode],adjnode});
-                    }
-                    d[adjnode]=di+weight;
-                    st.insert({d[adjnode],adjnode});
+                    // No need to erase anything, just update and push
+                    d[adjnode] = di + weight;
+                    pq.push({d[adjnode], adjnode});
                 }
             }
         }
         
-            for(int i = 1 ; i< n+1 ; i++){
-                if(d[i]==INT_MAX){
-                    return -1;
-                }
+        for(int i = 1; i < n + 1; i++){
+            if(d[i] == INT_MAX){
+                return -1;
             }
+        }
 
-            int max = *max_element(d.begin(),d.end());
-            return max;
-        
+        // Renamed to max_time to prevent shadowing std::max
+        int max_time = *max_element(d.begin(), d.end());
+        return max_time;
     }
 };
